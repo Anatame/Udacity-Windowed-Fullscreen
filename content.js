1196b9f914ff93338c87614f7f15d02870ca6757
Anatame <anata.me97@gmail.com>
2021-11-16 15:19:21 +0530

    Now resizes all video windows present in current page

--- content.js
+++ content.js
@@ -16,21 +16,27 @@ chrome.runtime.onMessage.addListener(
 
 function script() {
 
-    let floatDiv = document.querySelector(".index--atom--lmAIo")
+  let floatDivs = document.querySelectorAll(".index--atom--lmAIo")
+
+  floatDivs.forEach(floatDiv => {
     floatDiv.style.height = "100vh"
     floatDiv.style.width = "100%"
     floatDiv.style.margin = "0"
     floatDiv.style.padding = "0"
 
-    //remove the extra styling class, this one causes all stupid issues bithy class
+    //remove the extra styling class, this one causes all stupid issues bitchy class
     floatDiv.classList.remove('layout--content--3Smmq')
+  })
+   
 
     document.querySelector('.index--container--2OwOl').style.margin = '0'
 }
 
 function revert() {
-  let floatDiv = document.querySelector(".index--atom--lmAIo")
+  let floatDivs = document.querySelectorAll(".index--atom--lmAIo")
+  floatDivs.forEach(floatDiv => { 
+    floatDiv.removeAttribute('style')
+    floatDiv.classList.add('layout--content--3Smmq')
+  })
   document.querySelector('.index--container--2OwOl').style.margin = '25px'
-  floatDiv.removeAttribute('style')
-  floatDiv.classList.add('layout--content--3Smmq')
 }
